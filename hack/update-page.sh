17d3b2817ccaa57082411b9a7f40b0a3391d1aeb
Rick Rackow <rick+github@rackow.io>
2025-11-21 14:25:26 +0100

    feat(workflow): generate site automatically

---
+++ hack/update-page.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+make
+if [[ $(git status --porcelain) ]]; then
+  git status
+  git diff
+  git add .
+  git config --global user.name "${GITHUB_ACTOR}"
+  git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  git commit -m "docs(licenses,usage): Update docs & licenses"
+  git push origin main
+fi
